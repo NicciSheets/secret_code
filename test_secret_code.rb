974a8d14d0877f76ccbf5c3fbed157c60046d65d
NicciSheets <39924081+NicciSheets@users.noreply.github.com>
2018-07-03 00:24:06 -0400

    Delete test_secret_code.rb

--- test_secret_code.rb
+++
@@ -1,9 +0,0 @@
-require "minitest/autorun"
-require_relative "secret_code.rb"
-
-class TestSecretCode < Minitest::Test 
-
-	def test_assert_returns_as_array
-		assert_equal([], secret_code().class)
-	end
-end
