a3f93e5a7d17c113c511fe4ab20428229f3a89a0
Nicci Sheets <niccisheets@aol.com>
2018-07-03 09:11:56 -0400

    trying to figure out how to get the characters back from the ordinal number

--- test_secret_code.rb
+++ test_secret_code.rb
@@ -4,10 +4,22 @@ require_relative "secret_code.rb"
 class TestSecretCode < Minitest::Test 
 
 	# def test_assert_returns_as_array
-	# 	assert_equal(Array, secret_code("Nicci is so smart!").class)
+	# 	assert_equal(Array, secret_code().class)
 	# end
 
-	def test_assert_array_formatted_to_string
-		assert_equal(String, secret_code("Nicci is so smart!").class)
+	# # # def test_assert_array_formatted_to_string
+	# # 	assert_equal(String, secret_code("Nicci is so smart!").class)
+	# # end
+
+	# def test_assert_all_characters_lowercase
+	# 	assert_equal("nicci is so smart!", secret_code("Nicci is so smart!"))
+	# end
+
+	# def test_assert_z_returns_as_e
+	# 	assert_equal(["eeee"], secret_code2("zzZZ"))
+	# end
+	def test_assert_character_returns_as_plus_5_ordinal_values
+		assert_equal(["snhhnnxxtxrfwy"], secret_code("Nicci is so smart"))
 	end
+
 end
