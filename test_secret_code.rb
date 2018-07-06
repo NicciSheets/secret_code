c73ad48fd047ca716df878d1061bacf5fdc0d9e1
Nicci Sheets <niccisheets@aol.com>
2018-07-06 15:09:21 -0400

    starting over again, failed test for prepping user text for secret message

--- test_secret_code.rb
+++ test_secret_code.rb
@@ -3,30 +3,12 @@ require_relative "secret_code.rb"
 
 class TestSecretCode < Minitest::Test 
 
-	# def test_assert_returns_as_array
-	# 	assert_equal(Array, secret_code().class)
-	# end
-
-	# # # def test_assert_array_formatted_to_string
-	# # 	assert_equal(String, secret_code("Nicci is so smart!").class)
-	# # end
-
-	# def test_assert_all_characters_lowercase
-	# 	assert_equal("nicci is so smart!", secret_code("Nicci is so smart!"))
-	# end
-
-	# def test_assert_z_returns_as_e
-	# 	assert_equal(["eeee"], secret_code2("zzZZ"))
-	# end
-	# def test_assert_character_returns_as_plus_5_ordinal_values
-	# 	assert_equal("snhhn\%nx\%xt\%xrfwy", secret_code("Nicci is so smart"))
-	# end
-
-	def test_assert_returns_correct_letter
-		assert_equal("a", secret_code())
+	
+	def test_assert_ready_text_for_encryption
+		assert_equal("idislikesecretcodes", ready_text("I dislike secret codes."))
 	end
 
-	def test_assert_input_of_v
-		assert_equal("v", code_word("v"))
-	end
+	
 end
+
+#message : "I dislike secret codes."
