ad89bcceb266aa4e57bbeaf6f92e1bc3e098ed96
Nicci Sheets <niccisheets@aol.com>
2018-07-06 16:54:36 -0400

    failed test for indexing alphabet

--- secret_code.rb
+++ secret_code.rb
@@ -8,8 +8,18 @@ def text2char_arr(message)
 end
 
 
-def alphabet_index()
+def alphabet_index(letters)
+	# letters.to_a
+	# letter_arr = []
+	# counter = 0
+	# letters.map.each_with_index do |letter|
+	# 	if letter == "a"
+	# 		letter_arr << counter
+	# 	end 
+	# counter = counter + 1
+	# end
 end
+
 
 
 
@@ -17,3 +27,5 @@ end
 
 #puts ready_text("I dislike secret codes.")
 #p text2char_arr("idislikesecretcodes")
+#letters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"
+#p alphabet_index(letters)
--- test_secret_code.rb
+++ test_secret_code.rb
@@ -13,7 +13,8 @@ class TestSecretCode < Minitest::Test
 	end
 
 	def test_assert_indexing_regular_alphabet
-		assert_equal(["a".."z"], alphabet_index())
+		letters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"
+		assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"], alphabet_index(letters))
 	end
 
 end
